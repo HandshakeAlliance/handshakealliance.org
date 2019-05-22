6bc8ef5a1ee8356832f49fc030d1d095c164286b
Shane Jann <shanejann@gmail.com>
2019-05-21 22:02:58 -0500

    adding scss files and changing dark themes

---
+++ src/components/Hero/Hero.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import styled from 'styled-components';
+
+export default class Hero extends Component {
+
+  render() {
+    return (
+      <div>
+        This is a Hero
+      </div>
+    )
+  }
+}
---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import styled from 'styled-components';
+
+export default class Navbar extends Component {
+
+  render() {
+    return (
+      <div>
+        This is a navbar
+      </div>
+    )
+  }
+}
