857e5328848355e180f83ab7eecf1254264375a5
Shane Jann <shanejann@gmail.com>
2019-05-22 19:05:30 -0500

    adding simple maps

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.scss';
 
 import {ThemeProvider} from 'styled-components';
--- src/components/Navbar/styled-components.jsx
+++ src/components/Navbar/styled-components.jsx
@@ -2,7 +2,7 @@ import styled from 'styled-components';
 
 export const Wrapper = styled.div`
   align-items: center;
-  background: red;
+  background: #262626;
   display: flex;
   height: 60px;
   justify-content: space-between;
