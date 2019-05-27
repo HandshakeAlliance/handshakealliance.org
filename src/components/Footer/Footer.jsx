661277109b4e0cf82008a35ff3af1a90749ecd6a
Shane Jann <shanejann@gmail.com>
2019-05-27 09:58:52 -0500

    started adding the footer icons (not good yet)

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,10 +1,24 @@
 import React, { Component } from 'react';
+import styled from 'styled-components';
+
+const Logolink = styled.a`
+  color: #fff;
+  width: 48px;
+`;
 
 export default class FooterComponent extends Component {
   render() {
     return (
       <div>
-        I am a footer
+        <Logolink href="https://github.com/HandshakeAlliance" className="link" target="_blank" rel="noopener noreferrer">
+          <i className="fab fa-github-alt"></i>
+        </Logolink>
+        <Logolink href="https://twitter.com/HNSalliance" className="link" target="_blank" rel="noopener noreferrer">
+          <i className="fa-twitter"></i>
+        </Logolink>
+        <Logolink href="https://medium.com/@handshakealliance" className="link" target="_blank" rel="noopener noreferrer">
+          <i className="fab fa-medium-m"></i>
+        </Logolink>
       </div>
     )
   }
