61d71b504e36883032d777e4850f24218e877916
Shane Jann <shanejann@gmail.com>
2019-05-28 08:06:46 -0500

    adding footer changes

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,25 +1,28 @@
 import React, { Component } from 'react';
 import styled from 'styled-components';
+import * as Footer from './styled-components';
 
-const Logolink = styled.a`
-  color: #fff;
-  width: 48px;
-`;
+
 
 export default class FooterComponent extends Component {
   render() {
     return (
-      <div>
-        <Logolink href="https://github.com/HandshakeAlliance" className="link" target="_blank" rel="noopener noreferrer">
-          <i className="fab fa-github-alt"></i>
-        </Logolink>
-        <Logolink href="https://twitter.com/HNSalliance" className="link" target="_blank" rel="noopener noreferrer">
-          <i className="fa-twitter"></i>
-        </Logolink>
-        <Logolink href="https://medium.com/@handshakealliance" className="link" target="_blank" rel="noopener noreferrer">
-          <i className="fab fa-medium-m"></i>
-        </Logolink>
-      </div>
+      <Footer.Wrapper>
+        {/* TODO Consider adding backlinks to other sites here... Much like our other sites' footers */}
+
+        <Footer.FaContainer>
+          <Footer.Fa href="https://github.com/HandshakeAlliance" className="link" target="_blank" rel="noopener noreferrer">
+            <i className="fab fa-github-alt fa-2x"></i>
+          </Footer.Fa>
+          <Footer.Fa href="https://twitter.com/HNSalliance" className="link" target="_blank" rel="noopener noreferrer">
+            <i className="fab fa-twitter fa-2x"></i>
+          </Footer.Fa>
+          <Footer.Fa href="https://medium.com/@handshakealliance" className="link" target="_blank" rel="noopener noreferrer">
+            <i className="fab fa-medium-m fa-2x"></i>
+          </Footer.Fa>
+        </Footer.FaContainer>
+
+      </Footer.Wrapper>
     )
   }
 }
--- src/components/Footer/styled-components.jsx
+++ src/components/Footer/styled-components.jsx
@@ -1,3 +1,20 @@
 import styled from 'styled-components';
 
-export const Wrapper = styled.div``;
+export const Wrapper = styled.div`
+  margin: 24px;
+`;
+
+export const FaContainer = styled.div`
+  display: flex;
+  justify-content: center;
+`;
+
+export const Fa = styled.a`
+  color: #fff;
+  padding: 24px;
+  width: 48px;
+
+  &:hover {
+    color: #693afa;
+  }
+`;
