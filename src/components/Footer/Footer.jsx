569caa94746fd433571a10020403cde146cb2d45
Shane Jann <shanejann@gmail.com>
2019-05-28 19:51:27 -0500

    added some updates to the footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import styled from 'styled-components';
+import HA from '../Logos/HA';
 import * as Footer from './styled-components';
 
 
@@ -8,19 +8,53 @@ export default class FooterComponent extends Component {
   render() {
     return (
       <Footer.Wrapper>
-        {/* TODO Consider adding backlinks to other sites here... Much like our other sites' footers */}
+        <Footer.ActionContainer>
 
-        <Footer.FaContainer>
-          <Footer.Fa href="https://github.com/HandshakeAlliance" className="link" target="_blank" rel="noopener noreferrer">
+          <Footer.BlockColumn>
+            <HA color="#fff" />
+            <Footer.SubscribeLabel>Want to save the internet? Subscribe for notifications.</Footer.SubscribeLabel>
+            <Footer.SubscribeInput />
+            <Footer.SubscribeButton>Join Us</Footer.SubscribeButton>
+          </Footer.BlockColumn>
+
+          <Footer.BlockRow>
+            <Footer.LinkContainer>
+              <Footer.Header>Resources</Footer.Header>
+              <Footer.Link href="https://hnscan.com/">HNScan</Footer.Link>
+              <Footer.Link href="https://nameian.com/">Nameian</Footer.Link>
+              <Footer.Link href="https://handshakeacademy.org/en/">Academy</Footer.Link>
+              <Footer.Link href="https://handshake.community/">Community</Footer.Link>
+            </Footer.LinkContainer>
+            <Footer.LinkContainer>
+              <Footer.Header>Related</Footer.Header>
+              <Footer.Link href="https://github.com/HandshakeAlliance">Github</Footer.Link>
+              <Footer.Link href="https://handshakehash.com/">Pool</Footer.Link>
+              <Footer.Link href="https://handshakeacademy.org/en/">Learning</Footer.Link>
+              <Footer.Link href="https://handshake-org.github.io/api-docs/">Docs</Footer.Link>
+            </Footer.LinkContainer>
+          </Footer.BlockRow>
+
+        </Footer.ActionContainer>
+
+        <Footer.InfoContainer>
+          <Footer.TextContainer>
+            <Footer.Bold>CONTACT</Footer.Bold>
+            <span>engineering@urkel.io</span>
+          </Footer.TextContainer>
+        </Footer.InfoContainer>
+
+        <Footer.IconContainer>
+          <Footer.Fa href="https://github.com/HandshakeAlliance" target="_blank" rel="noopener noreferrer">
             <i className="fab fa-github-alt fa-2x"></i>
           </Footer.Fa>
-          <Footer.Fa href="https://twitter.com/HNSalliance" className="link" target="_blank" rel="noopener noreferrer">
+          <Footer.Fa href="https://twitter.com/HNSalliance" target="_blank" rel="noopener noreferrer">
             <i className="fab fa-twitter fa-2x"></i>
           </Footer.Fa>
-          <Footer.Fa href="https://medium.com/@handshakealliance" className="link" target="_blank" rel="noopener noreferrer">
+          <Footer.Fa href="https://medium.com/@handshakealliance" target="_blank" rel="noopener noreferrer">
             <i className="fab fa-medium-m fa-2x"></i>
           </Footer.Fa>
-        </Footer.FaContainer>
+        </Footer.IconContainer>
+
 
       </Footer.Wrapper>
     )
--- src/components/Footer/styled-components.jsx
+++ src/components/Footer/styled-components.jsx
@@ -4,7 +4,115 @@ export const Wrapper = styled.div`
   margin: 24px;
 `;
 
-export const FaContainer = styled.div`
+export const ActionContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: space-evenly;
+
+  @media(min-width: ${props => props.theme.breakpoint.desktopMin}) {
+    flex-direction: row;
+  }
+`;
+
+export const BlockColumn = styled.div`
+  align-items: flex-start;
+  display: flex;
+  flex-direction: column;
+`;
+
+export const BlockRow = styled.div`
+  display: flex;
+  flex-direction: column;
+
+  @media(min-width: ${props => props.theme.breakpoint.desktopMin}) {
+    flex-direction: row;
+  }
+`;
+
+export const SubscribeLabel = styled.div`
+  font-size: 14px;
+  margin-bottom: 4px;
+`;
+
+export const SubscribeInput = styled.input`
+  background: #363636;
+  border: 1.5px solid #696969;
+  color: #fafafa;
+  font-size: 16px;
+  margin-bottom: 8px;
+  outline: none;
+`;
+
+export const SubscribeButton = styled.button`
+  background: #693afa;
+  border: 1px solid #693afa;
+  border-radius: 1px;
+  color: #f2f2f2;
+  font-size: 14px;
+  padding: 8px;
+
+  &:hover {
+    background: #fafafa;
+    color: #262626;
+  }
+`;
+
+export const LinkContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  margin: 24px;
+  text-align: left;
+`;
+
+export const Header = styled.div`
+  font-weight: 600;
+  padding-bottom: 5px;
+  text-transform: uppercase;
+`;
+
+export const Link = styled.a`
+  font-size: 14px;
+  font-weight: 300px;
+
+  &:hover {
+    color: #693afa;
+    cursor: pointer;
+  }
+
+  &:visited {
+    color: #f2f2f2;
+    text-decoration-line: none;
+  }
+`;
+
+export const TextContainer = styled.div`
+  width: 75%;
+  max-width: 325px;
+  min-width: 260px;
+  margin-top: 10px;
+  font-size: 11pt;
+  text-align: center;
+`;
+
+export const Bold = styled.div`
+  font-weight: bold;
+  font-size: 10pt;
+  margin: 20px 0 0 0;
+  &.boldLink {
+    font-size: 11pt;
+    padding-bottom: 5px;
+  }
+`;
+
+export const InfoContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  height: auto;
+  align-items: center;
+`;
+
+export const IconContainer = styled.div`
   display: flex;
   justify-content: center;
 `;
