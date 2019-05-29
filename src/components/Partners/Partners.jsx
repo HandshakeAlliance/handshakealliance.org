5763370cf8441c2e1183abef96f86f80ca7b5bd8
Shane Jann <shanejann@gmail.com>
2019-05-28 23:17:04 -0500

    started adding components for Partners - got frustrated

--- src/components/Partners/Partners.jsx
+++ src/components/Partners/Partners.jsx
@@ -1,11 +1,46 @@
 import React, { Component } from 'react';
+// import Amentum from '../Logos/Amentum';
+import * as Partners from './styled-components';
 
 export default class PartnersComponent extends Component {
   render() {
     return (
-      <div>
-        I am a Partners
-      </div>
+      <Partners.Wrapper>
+        <Partners.LogoWrapper>
+          {/* <Amentum /> */}
+        </Partners.LogoWrapper>
+        <Partners.LogoWrapper>
+
+        </Partners.LogoWrapper>
+        <Partners.LogoWrapper>
+
+        </Partners.LogoWrapper>
+        <Partners.LogoWrapper>
+
+        </Partners.LogoWrapper>
+         {/* p.focusBody
+   .row.partnersSection
+     .col-xs-6.col-sm-4
+       .colWrapper
+         a.companyLink(href="https://amentum.org/" target="_target" rel="noopener noreferrer")
+           img#amentumLogo.grayscale(src="img/partnerAmentum.svg")
+     .col-xs-6.col-sm-4
+       .colWrapper
+         a.companyLink(href="https://provide.services/" target="_blank" rel="noopener noreferrer")
+           img#provideLogo.grayscale(src="img/partnerProvideDarkBlueBg.svg")
+     .col-xs-6.col-sm-4.last-xs
+       .colWrapper
+         a.companyLink(href="https://urkel.io/" target="_blank" rel="noopener noreferrer")
+           img#urkelLogo.grayscale(src="img/UrkelGlassesLogo.svg")
+     .col-xs-6.col-sm-4
+       .colWrapper
+         //- a.companyLink(href="#" rel="noopener noreferrer")
+         img#HNSimpleLogo.grayscale(src="img/partnerHNSimple.svg")
+     .col-xs-6.col-sm-4
+       .colWrapper
+         a.companyLink(href="https://namebase.io/" target="_blank" rel="noopener noreferrer")
+           img#namebaseLogo.grayscale(src="img/namebase.svg") */}
+      </Partners.Wrapper>
     )
   }
 }
