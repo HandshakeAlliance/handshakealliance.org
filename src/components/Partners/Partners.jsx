c8a24c3c4c22018003a44fdf0b1a68ae6277664a
Shane Jann <shanejann@gmail.com>
2019-06-07 23:51:13 -0500

    adding in HNSimple

--- src/components/Partners/Partners.jsx
+++ src/components/Partners/Partners.jsx
@@ -2,10 +2,11 @@ import React, { Component } from 'react';
 import * as Partners from './styled-components';
 
 import Amentum from '../Logos/Amentum';
-import { ReactComponent as Connext } from '../../img/connext.svg';
 import Namebase from '../Logos/Namebase';
 import Provide from '../Logos/Provide';
 import Urkel from '../Logos/Urkel';
+import { ReactComponent as Connext } from '../../img/connext.svg';
+import { ReactComponent as HNSimple } from '../../img/HNSimple.svg';
 export default class PartnersComponent extends Component {
   render() {
     return (
@@ -23,6 +24,9 @@ export default class PartnersComponent extends Component {
           </Partners.LogoWrapper>
           <Partners.LogoWrapper href="https://amentum.org/" target="_target" rel="noopener noreferrer">
             <Amentum height="70px" />
+          </Partners.LogoWrapper>
+          <Partners.LogoWrapper href="" target="_target" rel="noopener noreferrer">
+            <HNSimple />
           </Partners.LogoWrapper>
           <Partners.LogoWrapper href="https://provide.services/" target="_blank" rel="noopener noreferrer">
             <Provide height="70px" />
