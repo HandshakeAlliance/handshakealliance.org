05ff9a0b550fb8574b3af4920f90f88bf697b0dc
Shane Jann <shanejann@gmail.com>
2019-06-09 22:11:54 -0500

    added kyokan logo and rearranged to (hopefully) fit a little better

--- src/components/Partners/Partners.jsx
+++ src/components/Partners/Partners.jsx
@@ -7,6 +7,7 @@ import Provide from '../Logos/Provide';
 import { ReactComponent as UrkelLabs } from '../../img/UrkelLabs.svg';
 import { ReactComponent as Connext } from '../../img/connext.svg';
 import { ReactComponent as HNSimple } from '../../img/HNSimple.svg';
+import { ReactComponent as Kyokan } from '../../img/kyokan.svg';
 export default class PartnersComponent extends Component {
   render() {
     return (
@@ -16,8 +17,8 @@ export default class PartnersComponent extends Component {
         <Partners.Hr />
 
         <Partners.Logos>
-          <Partners.LogoWrapper href="https://connext.network/" target="_blank" rel="noopener noreferrer">
-            <Connext height="65px" />
+          <Partners.LogoWrapper href="https://amentum.org/" target="_target" rel="noopener noreferrer">
+            <Amentum height="75px" />
           </Partners.LogoWrapper>
           <Partners.LogoWrapper href="https://urkel.io/" target="_blank" rel="noopener noreferrer">
             <UrkelLabs height="80px" />
@@ -25,17 +26,19 @@ export default class PartnersComponent extends Component {
           <Partners.LogoWrapper href="" target="_target" rel="noopener noreferrer">
             <HNSimple />
           </Partners.LogoWrapper>
-          <Partners.LogoWrapper href="https://namebase.io/" target="_blank" rel="noopener noreferrer">
-            <Namebase height="35px" />
-          </Partners.LogoWrapper>
-          <Partners.LogoWrapper href="https://amentum.org/" target="_target" rel="noopener noreferrer">
-            <Amentum height="75px" />
-          </Partners.LogoWrapper>
           <Partners.LogoWrapper href="https://provide.services/" target="_blank" rel="noopener noreferrer">
             <Provide height="70px" />
           </Partners.LogoWrapper>
+          <Partners.LogoWrapper href="https://connext.network/" target="_blank" rel="noopener noreferrer">
+            <Connext height="65px" />
+          </Partners.LogoWrapper>
+          <Partners.LogoWrapper href="https://apexhns.com/" target="_blank" rel="noopener noreferrer">
+            <Kyokan />
+          </Partners.LogoWrapper>
+          <Partners.LogoWrapper href="https://namebase.io/" target="_blank" rel="noopener noreferrer">
+            <Namebase height="35px" />
+          </Partners.LogoWrapper>
         </Partners.Logos>
-
       </Partners.Wrapper>
     )
   }
