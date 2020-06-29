becc0cd77911ea317726ef324965bfea796c7c88
JGeiser9 <32041734+JGeiser9@users.noreply.github.com>
2020-06-29 10:36:29 -0500

    header: remove learn more button

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -9,7 +9,6 @@ export default class HeroComponent extends Component {
       <Hero.Wrapper>
         <Hero.Description>
           Working together to save the future of the internet
-          <Hero.LearnButton href="https://handshake.org/" target="_blank" rel="noopener noreferrer">Learn More</Hero.LearnButton>
         </Hero.Description>
         <Hero.Map>
           <NodeMapComponent />
