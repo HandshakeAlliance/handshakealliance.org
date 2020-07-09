a501b138e774166ea753706cf9145dcb48ceb3c1
JGeiser9 <32041734+JGeiser9@users.noreply.github.com>
2020-07-09 09:43:30 -0500

    calendar invite links - times are slightly off

--- src/components/Events/AddToCalendarButton.jsx
+++ src/components/Events/AddToCalendarButton.jsx
@@ -1,4 +1,5 @@
-import React, { useState } from "react";
+import React, { useState, useMemo } from "react";
+import moment from "moment-timezone";
 import styled from "styled-components";
 
 const ButtonWrapper = styled.div`
@@ -15,7 +16,7 @@ const Button = styled.button`
   min-width: 160px;
 `;
 
-const Dropdown = styled.div`
+const ButtonDropdown = styled.div`
   display: ${props => props.active ? "block" : "none"};
   position: absolute;
   background-color: #f1f1f1;
@@ -24,7 +25,7 @@ const Dropdown = styled.div`
   z-index: 1;
 `;
 
-const CalendarLink = styled.a`
+const Anchor = styled.a`
   color: black;
   padding: 12px 16px;
   text-decoration: none;
@@ -32,22 +33,112 @@ const CalendarLink = styled.a`
   &:hover { background-color: #ddd }
 `;
 
+// --- Helper Functions ---
+const makeUrl = (base, query) => Object.keys(query).reduce(
+  (accum, key, index) => {
+    const value = query[key];
+
+    if (value !== null) {
+      return `${accum}${index === 0 ? "?" : "&"}${key}=${encodeURIComponent(value)}`;
+    }
+    return accum;
+  },
+  base
+);
+
+const makeDuration = (data) => {
+  const minutes = Math.floor((+new Date(data?.start.dateTime) - +new Date(data?.end.dateTime)) / 60 / 1000);
+  return `${Math.floor(minutes / 60)}:${`0${minutes % 60}`.slice(-2)}`;
+};
+
+const makeTime = (time) => (`${moment(time).format("YYYYMMDD[T]HHmmss")}`);
+
+const makeUrls = (data) => ({
+  google: makeGoogleCalendarUrl(data),
+  outlook: makeOutlookCalendarUrl(data),
+  yahoo: makeYahooCalendarUrl(data),
+  ics: makeICSCalendarUrl(data)
+});
+
+const makeGoogleCalendarUrl = (data) => makeUrl("https://calendar.google.com/calendar/render", {
+  action: "TEMPLATE",
+  text: "Handshake Meeting",
+  details: data?.summary,
+  dates: `${makeTime(data?.start.dateTime)}/${makeTime(data?.end.dateTime)}`
+});
+
+// Need to Test
+const makeOutlookCalendarUrl = (data) => makeUrl("https://outlook.live.com/owa", {
+  rru: "addevent",
+  startdt: makeTime(data?.start.dateTime),
+  enddt: makeTime(data?.end.dateTime),
+  subject: "Handshake Meeting",
+  body: data?.summary,
+  allday: false,
+  uid: new Date().getTime().toString(),
+  path: "/calendar/view/Month"
+});
+
+// Need to test
+const makeYahooCalendarUrl = (data) => makeUrl("https://calendar.yahoo.com", {
+  v: 60,
+  view: "d",
+  type: 20,
+  title: "Handshake Meeting",
+  st: makeTime(data?.start.dateTime),
+  dur: makeDuration(data),
+  desc: data?.summary
+});
+
+const makeICSCalendarUrl = (data) => {
+  const components = [
+    "BEGIN:VCALENDAR",
+    "VERSION:2.0",
+    "BEGIN:VEVENT",
+    `URL:${document.URL}`,
+    `DTSTART:${makeTime(data?.start.dateTime)}`,
+    `DTEND:${makeTime(data?.end.dateTime)}`,
+    `SUMMARY:"Handshake Meeting"`,
+    `DESCRIPTION:${data?.summary}`,
+    "END:VEVENT",
+    "END:VCALENDAR"
+  ];
+
+  return encodeURI(`data:text/calendar;charset=utf8,${components.join("\n")}`);
+};
+
+// --- Functional Components ---
+function Calendar({ children, download = false, href }) {
+  return (
+    <Anchor download={download} href={href} target="_blank" rel="noopener noreferrer">
+      {children}
+    </Anchor>
+  )
+}
+
+function Dropdown({ active, urls }) {
+  console.log(urls);
+  return (
+    <ButtonDropdown active={active}>
+      <Calendar href={urls.ical} download>iCal</Calendar>
+      <Calendar href={urls.google}>Google</Calendar>
+      <Calendar href={urls.outlook}>Outlook</Calendar>
+      <Calendar href={urls.yahoo}>Yahoo</Calendar>
+    </ButtonDropdown>
+  )
+}
+
 export default function AddToCalendarButton({ data, value }) {
   const [showDropdown, setDropdown] = useState(false);
   const toggleDropdown = () => setDropdown(active => !active);
-  const closeDropdown = () => setDropdown(false);
-
-
+  const urls = useMemo(() => makeUrls(data), [data]);
 
   return (
     <ButtonWrapper>
-      <Button type="button" onClick={toggleDropdown} onBlur={closeDropdown}>{value}</Button>
-      <Dropdown active={showDropdown}>
-        <CalendarLink href="#">iCal</CalendarLink>
-        <CalendarLink href="#">Google</CalendarLink>
-        <CalendarLink href="#">Outlook</CalendarLink>
-        <CalendarLink href="#">Yahoo</CalendarLink>
-      </Dropdown>
+      {data && (
+        <Button type="button" onClick={toggleDropdown}>{value}</Button>
+      )}
+      {showDropdown && <Dropdown active={showDropdown} urls={urls} />}
     </ButtonWrapper>
   );
 }
