f8afd12eff2ddb9a2cbe175c7881bc64261850e6
JGeiser9 <32041734+JGeiser9@users.noreply.github.com>
2020-07-09 20:26:30 -0500

    implement react-add-to-calendar-hoc and clean up old helper functions

--- src/components/Events/AddToCalendarButton.jsx
+++ src/components/Events/AddToCalendarButton.jsx
@@ -1,14 +1,19 @@
-import React, { useState, useMemo } from "react";
+import React from "react";
+import { css } from "emotion";
+import styled from "@emotion/styled";
 import moment from "moment-timezone";
-import styled from "styled-components";
+import AddToCalendarHOC from 'react-add-to-calendar-hoc';
 
 const ButtonWrapper = styled.div`
   position: relative;
   display: inline-block;
-  min-width: 160px;
 `;
 
-const Button = styled.button`
+const componentStyles = css`
+  width: 100%;
+`;
+
+const buttonClassName = css`
   background: #f2f2f2;
   color: black;
   padding: 10px;
@@ -19,8 +24,7 @@ const Button = styled.button`
   min-width: 160px;
 `;
 
-const ButtonDropdown = styled.div`
-  display: ${props => props.active ? "block" : "none"};
+const dropdownStyles = css`
   position: absolute;
   background-color: #f1f1f1;
   min-width: 160px;
@@ -28,7 +32,7 @@ const ButtonDropdown = styled.div`
   z-index: 1;
 `;
 
-const Anchor = styled.a`
+const linkStyles = css`
   color: black;
   padding: 12px 16px;
   text-decoration: none;
@@ -36,112 +40,56 @@ const Anchor = styled.a`
   &:hover { background-color: #ddd }
 `;
 
-// --- Helper Functions ---
-const makeUrl = (base, query) => Object.keys(query).reduce(
-  (accum, key, index) => {
-    const value = query[key];
-
-    if (value !== null) {
-      return `${accum}${index === 0 ? "?" : "&"}${key}=${encodeURIComponent(value)}`;
-    }
-    return accum;
-  },
-  base
-);
-
-const makeDuration = (data) => {
-  const minutes = Math.floor((+new Date(data?.start.dateTime) - +new Date(data?.end.dateTime)) / 60 / 1000);
-  return `${Math.floor(minutes / 60)}:${`0${minutes % 60}`.slice(-2)}`;
-};
-
-const makeTime = (time) => (`${moment(time).format("YYYYMMDD[T]HHmmss")}`);
-
-const makeUrls = (data) => ({
-  google: makeGoogleCalendarUrl(data),
-  outlook: makeOutlookCalendarUrl(data),
-  yahoo: makeYahooCalendarUrl(data),
-  ics: makeICSCalendarUrl(data)
-});
-
-const makeGoogleCalendarUrl = (data) => makeUrl("https://calendar.google.com/calendar/render", {
-  action: "TEMPLATE",
-  text: "Handshake Meeting",
-  details: data?.summary,
-  dates: `${makeTime(data?.start.dateTime)}/${makeTime(data?.end.dateTime)}`
-});
-
-// Need to Test
-const makeOutlookCalendarUrl = (data) => makeUrl("https://outlook.live.com/owa", {
-  rru: "addevent",
-  startdt: makeTime(data?.start.dateTime),
-  enddt: makeTime(data?.end.dateTime),
-  subject: "Handshake Meeting",
-  body: data?.summary,
-  allday: false,
-  uid: new Date().getTime().toString(),
-  path: "/calendar/view/Month"
-});
-
-const makeYahooCalendarUrl = (data) => makeUrl("https://calendar.yahoo.com", {
-  v: 60,
-  view: "d",
-  type: 20,
-  title: "Handshake Meeting",
-  st: makeTime(data?.start.dateTime),
-  dur: makeDuration(data),
-  desc: data?.summary
-});
-
-// TODO: this needs refining
-const makeICSCalendarUrl = (data) => {
-  const components = [
-    "BEGIN:VCALENDAR",
-    "VERSION:2.0",
-    "BEGIN:VEVENT",
-    `URL:${document.URL}`,
-    `DTSTART:${makeTime(data?.start.dateTime)}`,
-    `DTEND:${makeTime(data?.end.dateTime)}`,
-    `SUMMARY:"Handshake Meeting"`,
-    `DESCRIPTION:${data?.summary}`,
-    "END:VEVENT",
-    "END:VCALENDAR"
-  ];
-
-  return encodeURI(`data:text/calendar;charset=utf8,${components.join("\n")}`);
-};
-
-// --- Functional Components ---
-function Calendar({ children, download = false, href }) {
+const Button = ({ children, onClick }) => {
   return (
-    <Anchor download={download} href={href} target="_blank" rel="noopener noreferrer">
+    <button className={buttonClassName} onClick={onClick}>
       {children}
-    </Anchor>
-  )
+    </button>
+  );
 }
 
-function Dropdown({ active, urls }) {
-  console.log(urls);
+const Dropdown = ({ children }) => {
   return (
-    <ButtonDropdown active={active}>
-      <Calendar href={urls.ics} download>iCal</Calendar>
-      <Calendar href={urls.google}>Google</Calendar>
-      <Calendar href={urls.outlook}>Outlook</Calendar>
-      <Calendar href={urls.yahoo}>Yahoo</Calendar>
-    </ButtonDropdown>
-  )
+    <div className={dropdownStyles}>
+      {children}
+    </div>
+  );
 }
 
-export default function AddToCalendarButton({ data, value }) {
-  const [showDropdown, setDropdown] = useState(false);
-  const toggleDropdown = () => setDropdown(active => !active);
-  const urls = useMemo(() => makeUrls(data), [data]);
+const MakeEvent = ({ event }) => {
+  const startDatetime = moment(event.start.dateTime).utc();
+  const endDatetime = moment(event.end.dateTime).utc();
+  // const duration = moment.duration(endDatetime.diff(startDatetime)).as("hours");
 
+  // Note: hard coding duration for the time being until PR gets merged
+  // in 'react-add-to-calendar-hoc'
+  const calendarEvent = {
+    description: event.summary,
+    duration: "0015",
+    endDatetime: endDatetime.format("YYYYMMDDTHHmmssZ"),
+    location: "TBD",
+    startDatetime: startDatetime.format("YYYYMMDDTHHmmssZ"),
+    title: "Handshake Alliance Meeting",
+  }
+
+  const AddToCalendarDropdown = AddToCalendarHOC(Button, Dropdown);
+
+  return (
+    <AddToCalendarDropdown
+      className={componentStyles}
+      linkProps={{
+        className: linkStyles,
+      }}
+      event={calendarEvent}
+      filename="handshake_meeting"
+    />
+  );
+}
+
+export default function AddToCalendarButton({ event }) {
   return (
     <ButtonWrapper>
-      {data && (
-        <Button type="button" onClick={toggleDropdown}>{value}</Button>
-      )}
-      {showDropdown && <Dropdown active={showDropdown} urls={urls} />}
+      <MakeEvent event={event} />
     </ButtonWrapper>
   );
 }
