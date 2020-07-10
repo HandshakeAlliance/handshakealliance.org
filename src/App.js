d3874d9caa46099723081598f9ba40cd5116f8e1
JGeiser9 <32041734+JGeiser9@users.noreply.github.com>
2020-07-10 13:33:08 -0500

    style: move single sass style to css-in-js with emotion

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
 import React from 'react';
 import ReactGA from 'react-ga';
-import './App.scss';
-
 import { ThemeProvider } from 'styled-components';
-
-import NavbarComponent from './components/Navbar/Navbar.jsx';
-import HeroComponent from './components/Hero/Hero.jsx';
-import ObjectiveComponent from './components/Objective/Objective.jsx';
-import ToolsComponent from './components/Tools/Tools.jsx';
-import ExchangesComponent from "components/Exchanges/Exchanges";
-import DonationsComponent from './components/Donations/Donations.jsx';
-import FooterComponent from './components/Footer/Footer.jsx';
-import EventsComponent from 'components/Events/Events';
-
-import themes from './theme-variables.js';
-import { breakpoints } from './breakpoint-variables.js';
+import styled from "@emotion/styled";
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
+// Components
+import NavbarComponent from 'components/Navbar/Navbar.jsx';
+import HeroComponent from 'components/Hero/Hero.jsx';
+import ObjectiveComponent from 'components/Objective/Objective.jsx';
+import ToolsComponent from 'components/Tools/Tools.jsx';
+import ExchangesComponent from "components/Exchanges/Exchanges";
+import DonationsComponent from 'components/Donations/Donations.jsx';
+import FooterComponent from 'components/Footer/Footer.jsx';
+import EventsComponent from 'components/Events/Events';
+
+import themes from './theme-variables.js';
+// TODO: replace with ucl breakpoints
+import { breakpoints } from './breakpoint-variables.js';
+
+// TODO: remove and fix breakpoints
 const theme = {
   breakpoint: breakpoints,
   ...themes
@@ -28,11 +30,18 @@ function initializeReactGA() {
   ReactGA.pageview('/');
 }
 
-function App() {
+// TODO: I feel like this could just be a flex
+const Content = styled.div`
+  display: flex;
+  flex-direction: column;
+  text-align: center;
+`;
+
+export default function App() {
   initializeReactGA();
   return (
     <ThemeProvider theme={theme}>
-      <div className="App">
+      <Content>
         <NavbarComponent />
         <HeroComponent />
         <ObjectiveComponent />
@@ -45,9 +54,7 @@ function App() {
           autoClose={3000}
           hideProgressBar={true}
         />
-      </div>
+      </Content>
     </ThemeProvider>
   );
 }
-
-export default App;
