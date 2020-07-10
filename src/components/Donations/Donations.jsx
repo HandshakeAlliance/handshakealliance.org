48d19a904c9220cb957131be455ede1f87c9089c
JGeiser9 <32041734+JGeiser9@users.noreply.github.com>
2020-07-10 14:10:56 -0500

    combine donation component file and styles

--- src/components/Donations/Donations.jsx
+++ src/components/Donations/Donations.jsx
@@ -1,11 +1,38 @@
 import React from 'react';
 import { toast } from 'react-toastify';
-import { Flex } from '@urkellabs/ucl';
+import { Flex, breakpoint } from '@urkellabs/ucl';
+import styled from "@emotion/styled";
 
 // Components
-import * as Donations from './styled-components';
 import SectionWrapper from 'components/shared/SectionWrapper';
 import SectionHeader from 'components/shared/SectionHeader';
+
+export const Body = styled.p`
+  max-width: 400px;
+  margin-bottom: 35px;
+  ${breakpoint.tablet} {
+    margin-bottom: 30px;
+    max-width: 600px;
+  }
+`;
+
+export const Address = styled.input`
+  background: #363636;
+  border: 1.5px solid #696969;
+  border-radius: 2px;
+  color: #fafafa;
+  font-size: 16px;
+  margin-bottom: 12px;
+  min-width: 280px;
+  overflow: hidden;
+  outline: none;
+  padding: 8px;
+  text-align: center;
+  text-overflow: ellipsis;
+  white-space: nowrap;
+  &::selection { background: none; }
+  &:hover { cursor: pointer; }
+`;
 
 export default function DonationsComponent() {
   const handleClick = event => {
@@ -20,8 +47,8 @@ export default function DonationsComponent() {
     <SectionWrapper backgroundColor="#252525" color="#f2f2f2">
       <Flex columns justify="center" align="center">
         <SectionHeader>Support Development</SectionHeader>
-        <Donations.Body>All proceeds will go directly towards the teams developing the infrastructure for the Handshake protocol</Donations.Body>
-        <Donations.Address onClick={handleClick} readOnly value="37jov2n9YKofVjCQX43MVSSc36GeL2RSss" />
+        <Body>All proceeds will go directly towards the teams developing the infrastructure for the Handshake protocol</Body>
+        <Address onClick={handleClick} readOnly value="37jov2n9YKofVjCQX43MVSSc36GeL2RSss" />
       </Flex>
     </SectionWrapper>
   )
