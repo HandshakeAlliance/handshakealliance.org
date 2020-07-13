c658051e8b3afe14cc05cffe73cb6fadcdfcafc5
JGeiser9 <32041734+JGeiser9@users.noreply.github.com>
2020-07-13 16:35:06 -0500

    handshake alliance config file

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,31 +1,49 @@
-module.exports = {
-  siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+let path = require("path");
+require("dotenv").config({
+  path: `.env.${process.env.NODE_ENV}`
+});
+
+let plugins = [
+  `gatsby-plugin-react-helmet`,
+  `gatsby-transformer-sharp`,
+  `gatsby-plugin-sharp`,
+  {
+    resolve: `gatsby-source-filesystem`,
+    options: {
+      name: `images`,
+      path: `${__dirname}/src/images`,
+    },
   },
-  plugins: [
-    `gatsby-plugin-react-helmet`,
-    {
-      resolve: `gatsby-source-filesystem`,
-      options: {
-        name: `images`,
-        path: `${__dirname}/src/images`,
-      },
+  {
+    resolve: `gatsby-plugin-manifest`,
+    options: {
+      name: `Handshakealliance.org`,
+      short_name: `Handshakealliance.org`,
+      start_url: `/`,
+      background_color: `#fff`,
+      theme_color: `#693AFA`,
+      display: `standalone`,
+      icon: `src/images/ha-logo.png`,
     },
-    `gatsby-transformer-sharp`,
-    `gatsby-plugin-sharp`,
-    {
-      resolve: `gatsby-plugin-manifest`,
-      options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
-        start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
-        display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
-      },
-    },
-  ],
-}
+  },
+  {
+    resolve: "gatsby-plugin-root-import",
+    options: {
+      src: path.join(__dirname, "src"),
+      components: path.join(__dirname, "src/components"),
+      images: path.join(__dirname, "src/images")
+    }
+  },
+]
+
+module.exports = {
+  pathPrefix: `/`,
+  siteMetadata: {
+    title: "Handshakealliance.org",
+    description: `Handshake is a decentralized naming and certificate authority where every peer is validating and in charge of managing the root DNS naming zone`,
+    url: `https://handshakealliance.org`,
+    siteUrl: `https://handshakealliance.org`,
+    author: `@HandshakeAlliance`
+  },
+  plugins: plugins
+};
