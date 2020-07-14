2b9a0e21735a558bb1b3c6dd705fdc264e964846
JGeiser9 <32041734+JGeiser9@users.noreply.github.com>
2020-07-14 16:58:57 -0500

    initial navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,21 +1,56 @@
 import React from "react"
 import styled from "styled-components"
-import { Flex } from "@urkellabs/ucl"
+import { NavBar, Flex, breakpoint } from "@urkellabs/ucl"
 
 // Components
 import NavLogo from "components/img/NavLogo"
 
-const NavigationWrapper = styled.nav`
-  height: 60px;
-  width: 100%;
-  border: 1px solid red;
-`;
+const Wrapper = styled(NavBar)`
+  box-shadow: 0px 5px 10px 0px var(--color-gray-2);
+  border-bottom: none;
+`
+
+const NavbarContainer = styled(NavBar.Container)`
+  ${breakpoint.mobile} {
+    width: 100%;
+  }
+`
+
+const NavbarBrand = styled(NavBar.Brand)`
+  margin-right: 25px;
+`
 
 const Navbar = () => {
   return (
-    <NavigationWrapper>
-      <div>Navbar</div>
-    </NavigationWrapper>
+    <Wrapper height="75px" >
+      <NavbarContainer>
+        <NavbarBrand>
+          <Flex align="center">
+            <NavLogo />
+            <NavBar.Burger />
+          </Flex>
+        </NavbarBrand>
+        <NavBar.Menu>
+          <NavBar.Start>
+            <NavBar.Item>Handshake</NavBar.Item>
+            <NavBar.Item hoverable dropdown>
+              <NavBar.Link>Individuals</NavBar.Link>
+              <NavBar.Dropdown>
+                <NavBar.Item>Link</NavBar.Item>
+                <NavBar.Item>Link</NavBar.Item>
+              </NavBar.Dropdown>
+            </NavBar.Item>
+            <NavBar.Item hoverable dropdown>
+              <NavBar.Link>Developers</NavBar.Link>
+              <NavBar.Dropdown>
+                <NavBar.Item>Link</NavBar.Item>
+                <NavBar.Item>Link</NavBar.Item>
+              </NavBar.Dropdown>
+            </NavBar.Item>
+          </NavBar.Start>
+        </NavBar.Menu>
+      </NavbarContainer>
+    </Wrapper>
   )
 }
 
