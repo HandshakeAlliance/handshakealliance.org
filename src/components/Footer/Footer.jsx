a2b1cb7ce773ddd44e35e5d0c2d7f753c8542f9f
JGeiser9 <32041734+JGeiser9@users.noreply.github.com>
2020-07-15 14:39:25 -0500

    clear warning for unused Component

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { Flex, Header, breakpoint } from "@urkellabs/ucl";
 import styled from "@emotion/styled";
 import AllianceDesktopLogo from '../Logos/AllianceDesktopLogo';
