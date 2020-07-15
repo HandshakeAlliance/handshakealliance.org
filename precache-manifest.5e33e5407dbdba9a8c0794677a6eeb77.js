32ed8328d580afa18d0a4c81ff8b3081c66fd2da
JGeiser9 <32041734+JGeiser9@users.noreply.github.com>
2020-07-15 14:41:25 -0500

    Updates

---
+++ precache-manifest.5e33e5407dbdba9a8c0794677a6eeb77.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4ee524f0fd1e5c05e642e4bf9e67f160",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2d3ccc175ce38285fa0a",
+    "url": "/static/css/2.f42e59c9.chunk.css"
+  },
+  {
+    "revision": "ad23ce60d03f287c3a2a",
+    "url": "/static/css/main.8c500b36.chunk.css"
+  },
+  {
+    "revision": "2d3ccc175ce38285fa0a",
+    "url": "/static/js/2.f71079fb.chunk.js"
+  },
+  {
+    "revision": "54670cb69346c14995236390486d8fcd",
+    "url": "/static/js/2.f71079fb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ad23ce60d03f287c3a2a",
+    "url": "/static/js/main.fad07f9a.chunk.js"
+  },
+  {
+    "revision": "d8883cefa273d9c4806d",
+    "url": "/static/js/runtime-main.7b93c697.js"
+  },
+  {
+    "revision": "71a6149eb178f019994584f1a7a00242",
+    "url": "/static/media/adoption.71a6149e.svg"
+  },
+  {
+    "revision": "10578ac93b560cef0c7091f501ea1a74",
+    "url": "/static/media/bobwallet.10578ac9.svg"
+  },
+  {
+    "revision": "665c404356e69e76b396d598e8e1cf25",
+    "url": "/static/media/community.665c4043.svg"
+  },
+  {
+    "revision": "46cb8f5663689facf5faabfd8e4db87d",
+    "url": "/static/media/educate.46cb8f56.svg"
+  },
+  {
+    "revision": "d622a140f28d452fb6e2df91610e1e4c",
+    "url": "/static/media/github.d622a140.svg"
+  },
+  {
+    "revision": "1250696259c79fc5d9cb8f2a923d9210",
+    "url": "/static/media/support.12506962.svg"
+  }
+]);
