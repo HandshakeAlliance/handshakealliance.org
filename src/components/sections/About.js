5df51e1467d233d00b09cb2b918fee5a05a09c9d
JGeiser9 <32041734+JGeiser9@users.noreply.github.com>
2020-07-15 16:05:17 -0500

    replace span with ucl header component

--- src/components/sections/About.js
+++ src/components/sections/About.js
@@ -1,5 +1,6 @@
 import React from "react";
 import styled from "styled-components";
+import { Header } from "@urkellabs/ucl";
 
 // Components
 import SectionHeader from "components/shared/SectionHeader";
@@ -12,7 +13,7 @@ const About = () => {
   return (
     <Wrapper>
       <SectionHeader small align="center" title="Handshake is a decentralized, permissionless naming protocol">
-        <span>where every peer validates and manages the root DNS naming zone for an alternative to existing certificate authorities and naming systems</span>
+        <Header xsmall>every peer on the network validates and manages the root DNS naming zone for an alternative to existing certificate authorities and naming systems</Header>
       </SectionHeader>
     </Wrapper>
   )
