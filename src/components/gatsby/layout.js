c99aca1e2790e51f76ed71eb2a9d084f6bd8219c
JGeiser9 <32041734+JGeiser9@users.noreply.github.com>
2020-07-15 12:24:58 -0500

    clear todo of moving lightColors to its own theme file

--- src/components/gatsby/layout.js
+++ src/components/gatsby/layout.js
@@ -1,24 +1,10 @@
 import React from "react";
-import styled, { css } from "styled-components";
+import styled from "styled-components";
 import { GlobalStyles } from "@urkellabs/ucl";
 
 // Components
 import Navbar from "components/navigation/Navbar";
-
-// @TODO: move to its own file and decide on final theme color palette
-const lightColors = css`
-  --color-black: hsl(0, 0%, 0%);
-  --color-white: hsl(0, 0%, 100%);
-  --color-gray-1: hsl(0, 0%, 100%);
-  --color-gray-2: hsl(0, 0%, 95%);
-  --color-gray-3: hsl(0, 0%, 85%);
-  --color-gray-4: hsl(0, 0%, 70%);
-  --color-gray-5: hsl(0, 0%, 55%);
-  --color-gray-6: hsl(0, 0%, 40%);
-  --color-gray-7: hsl(0, 0%, 30%);
-  --color-gray-8: hsl(0, 0%, 15%);
-  --color-gray-9: hsl(0, 0%, 0%);
-`;
+import lightColors from "components/theme/lightColors";
 
 const Content = styled.main`
   width: 95%;
---
+++ src/components/theme/lightColors.js
@@ -0,0 +1,17 @@
+import { css } from "styled-components";
+
+const lightColors = css`
+  --color-black: hsl(0, 0%, 0%);
+  --color-white: hsl(0, 0%, 100%);
+  --color-gray-1: hsl(0, 0%, 100%);
+  --color-gray-2: hsl(0, 0%, 95%);
+  --color-gray-3: hsl(0, 0%, 85%);
+  --color-gray-4: hsl(0, 0%, 70%);
+  --color-gray-5: hsl(0, 0%, 55%);
+  --color-gray-6: hsl(0, 0%, 40%);
+  --color-gray-7: hsl(0, 0%, 30%);
+  --color-gray-8: hsl(0, 0%, 15%);
+  --color-gray-9: hsl(0, 0%, 0%);
+`;
+
+export default lightColors;
