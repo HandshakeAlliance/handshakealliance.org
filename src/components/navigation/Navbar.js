541e56dfabd6821d955bb4fd06f64aa2389529bf
JGeiser9 <32041734+JGeiser9@users.noreply.github.com>
2020-07-20 22:03:14 -0500

    navbar: add gatbsy links to page routes

--- src/components/navigation/Navbar.js
+++ src/components/navigation/Navbar.js
@@ -1,5 +1,6 @@
 import React from "react";
 import styled from "styled-components";
+import { Link } from "gatsby";
 import { NavBar, Flex, breakpoint } from "@urkellabs/ucl";
 
 // Components
@@ -8,6 +9,7 @@ import Logo from "components/img/Logo";
 const Wrapper = styled.div`
   width: 100%;
   position: fixed;
+  top: 0;
   background: var(--color-gray-1);
   z-index: 10;
 `;
@@ -80,41 +82,41 @@ const Navbar = ({ boxShadow }) => {
       <CustomNavbar height="75px" boxShadow={boxShadow}>
         <NavbarContainer>
           <NavbarBrand>
-            <Flex align="center">
+            <Flex align="center" as={Link} to="/">
               <Logo />
             </Flex>
             <NavBar.Burger />
           </NavbarBrand>
           <NavBar.Menu>
             <NavBar.Start>
-              <NavbarItem>Handshake</NavbarItem>
+              <NavbarItem as={Link} to="/">Handshake</NavbarItem>
               <NavbarLinkDropdown hoverable dropdown>
                 <NavBar.Link className="link">Individuals</NavBar.Link>
                 <NavBar.Dropdown>
-                  <NavbarItem>What is Handshake?</NavbarItem>
-                  <NavbarItem>What is HNS?</NavbarItem>
-                  <NavbarItem>Earn HNS</NavbarItem>
-                  <NavbarItem>Use Handshake</NavbarItem>
-                  <NavbarItem>Handshake Wallets</NavbarItem>
-                  <NavbarItem>Guides and Resources</NavbarItem>
+                  <NavbarItem as={Link} to="/what-is-handshake">What is Handshake?</NavbarItem>
+                  <NavbarItem as={Link} to="/what-is-hns">What is HNS?</NavbarItem>
+                  <NavbarItem as={Link} to="/earn-hns">Earn HNS</NavbarItem>
+                  <NavbarItem as={Link} to="/use-handshake">Use Handshake</NavbarItem>
+                  <NavbarItem as={Link} to="/handshake-wallets">Handshake Wallets</NavbarItem>
+                  <NavbarItem as={Link} to="/guides-and-resources">Guides and Resources</NavbarItem>
                 </NavBar.Dropdown>
               </NavbarLinkDropdown>
               <NavbarLinkDropdown hoverable dropdown>
                 <NavBar.Link className="link">Developers</NavBar.Link>
                 <NavBar.Dropdown>
-                  <NavbarItem>Get Started</NavbarItem>
-                  <NavbarItem>Developer Resources</NavbarItem>
-                  <NavbarItem>Handshake Whitepaper</NavbarItem>
-                  <NavbarItem>Developer Roadmap</NavbarItem>
+                  <NavbarItem as={Link} to="/get-started">Get Started</NavbarItem>
+                  <NavbarItem as={Link} to="/developer-resources">Developer Resources</NavbarItem>
+                  <NavbarItem as={Link} to="/handshake-whitepaper">Handshake Whitepaper</NavbarItem>
+                  <NavbarItem as={Link} to="/developer-roadmap">Developer Roadmap</NavbarItem>
                 </NavBar.Dropdown>
               </NavbarLinkDropdown>
               <NavbarLinkDropdown hoverable dropdown>
                 <NavBar.Link className="link">Community</NavBar.Link>
                 <NavBar.Dropdown>
-                  <NavbarItem>Blog</NavbarItem>
-                  <NavbarItem>How Can I Help?</NavbarItem>
-                  <NavbarItem>Developer Assets</NavbarItem>
-                  <NavbarItem>Support</NavbarItem>
+                  <NavbarItem as={Link} to="/blog">Blog</NavbarItem>
+                  <NavbarItem as={Link} to="/how-can-i-help">How Can I Help?</NavbarItem>
+                  <NavbarItem as={Link} to="/developer-assets">Developer Assets</NavbarItem>
+                  <NavbarItem as={Link} to="/support">Support</NavbarItem>
                 </NavBar.Dropdown>
               </NavbarLinkDropdown>
             </NavBar.Start>
