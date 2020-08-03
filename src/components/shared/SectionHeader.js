8cea3711b4d931d0829c6366c76fbdca35dbca97
JGeiser9 <32041734+JGeiser9@users.noreply.github.com>
2020-08-03 15:15:08 -0500

    conditional spacing for section headers depending on whether or not they have subtext/children

--- src/components/shared/SectionHeader.js
+++ src/components/shared/SectionHeader.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import styled from "styled-components";
-import { Flex, Header } from "@urkellabs/ucl";
+import { Flex, Header, Spacer, breakpoint } from "@urkellabs/ucl";
 
 const StyledHeader = styled(Header)`
   margin: 0 0 16px;
@@ -16,15 +16,15 @@ const Subtext = styled.div`
 
 const Underline = styled.div`
   border: 2px solid var(--color-primary-3);
-  margin-top: 50px;
   width: 100px;
 `;
 
-export default function SectionHeader({ children, title, align, ...rest }) {
+export default function SectionHeader({ children, title, ...rest }) {
   return (
-    <Flex columns align={align} mt={50} mb={50}>
+    <Flex columns align="center" mt={50} mb={50}>
       <StyledHeader {...rest}>{title}</StyledHeader>
       <Subtext>{children}</Subtext>
+      {children ? <Spacer px={50} /> : <Spacer />}
       <Underline />
     </Flex>
   );
