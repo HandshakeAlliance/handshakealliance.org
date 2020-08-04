e17f16d2211eb958d3f7214b7ded1e27f3c02630
JGeiser9 <32041734+JGeiser9@users.noreply.github.com>
2020-08-04 16:45:50 -0500

    convert template to mdx, sticky side table of contents, cleanup

--- src/templates/markdown-page-template.js
+++ src/templates/markdown-page-template.js
@@ -1,49 +1,80 @@
 import React from "react";
 import styled from "styled-components";
-import { graphql } from "gatsby";
-import { Row, Col, Flex, Hidden } from "@urkellabs/ucl";
+import { Link, graphql } from "gatsby";
+import { Row, Col, Flex } from "@urkellabs/ucl";
+import { MDXRenderer } from "gatsby-plugin-mdx"
 
 const Wrapper = styled.section`
   min-height: 380px;
   padding: 125px 0 75px 0;
+
+  /* Anchor scroll offset to account for the fixed
+  navbar - add other header sizes if needed */
+  h2::before {
+    display: block;
+    content: " ";
+    margin-top: -120px;
+    height: 120px;
+    visibility: hidden;
+    pointer-events: none;
+  }
 `;
 
-const SideNavigation = styled.div`
+const MarkdownBody = styled.div`
+  padding-right: 50px;
+`;
+
+const Sidenav = styled.aside`
+  position: sticky;
   border-left: 2px solid var(--color-gray-2);
-  padding: 10px;
-  margin-top: 20px;
-  min-height: 100px;
+  padding: 0px 10px;
+  margin-top: 22px;
+  min-height: 50px;
+  overflow-y: auto;
+  top: 150px;
+  bottom: 0;
+  right: 0;
 `;
 
-const SideNavLink = styled.a`
+const SidebarLink = styled(Link)`
   border-left: 2px solid transparent;
   margin-left: -12px;
   padding: 5px 0 5px 10px;
   color: var(--color-gray-5);
+  font-size: 14px;
 
   &:hover {
     border-color: var(--color-primary);
   }
 `;
 
+const BuildSidenavItems = ({ items }) => {
+  return items.map((item, i) => (
+    <React.Fragment key={i}>
+      <SidebarLink to={item.url}>{item.title}</SidebarLink>
+    </React.Fragment>
+  ));
+};
+
 export default function Template({ data }) {
-  const { markdownRemark } = data;
-  const { html, tableOfContents, frontmatter } = markdownRemark;
+  const { mdx } = data;
+  const { body, tableOfContents, frontmatter } = mdx;
 
   return (
     <Wrapper>
       <Row>
         <Col mobile={12} desktop={9.5}>
-          <h1>{frontmatter.title}</h1>
-          <div dangerouslySetInnerHTML={{ __html: html }} />
+          <MarkdownBody>
+            <h1>{frontmatter.title}</h1>
+            <MDXRenderer>{body}</MDXRenderer>
+          </MarkdownBody>
         </Col>
-        {/* @TODO: Hidden up to desktop */}
         <Col desktop={2.5}>
-          <SideNavigation>
+          <Sidenav>
             <Flex columns>
-              <div dangerouslySetInnerHTML={{ __html: tableOfContents }} />
+              <BuildSidenavItems items={tableOfContents.items} />
             </Flex>
-          </SideNavigation>
+          </Sidenav>
         </Col>
       </Row>
     </Wrapper>
@@ -52,9 +83,9 @@ export default function Template({ data }) {
 
 export const pageQuery = graphql`
   query($slug: String!) {
-    markdownRemark(frontmatter: { slug: { eq: $slug } }) {
-      html
-      tableOfContents(absolute: false, pathToSlugField: "frontmatter.path")
+    mdx(frontmatter: { slug: { eq: $slug } }) {
+      body
+      tableOfContents
       frontmatter {
         slug
         title
