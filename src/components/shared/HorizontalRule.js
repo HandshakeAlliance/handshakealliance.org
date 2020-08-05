89ae55ec2fec7f11470be3e8ad5c045b9536f3c4
JGeiser9 <32041734+JGeiser9@users.noreply.github.com>
2020-08-05 18:55:58 -0500

    adding proptypes to the horizontal rule component to control border width

--- src/components/shared/HorizontalRule.js
+++ src/components/shared/HorizontalRule.js
@@ -1,11 +1,22 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import styled from "styled-components";
 
 const StyledHorizontalRule = styled.hr`
-  border: 2px solid var(--color-gray-2);
+  border-width: ${props => props.borderWidth}px;
+  border-style: solid;
+  border-color: var(--color-gray-2);
   margin: 50px 0;
 `;
 
-export default function HorizontalRule() {
-  return <StyledHorizontalRule />
+export default function HorizontalRule({ borderWidth }) {
+  return <StyledHorizontalRule borderWidth={borderWidth} />
+}
+
+HorizontalRule.defaultProps = {
+  borderWidth: 1
+}
+
+HorizontalRule.propTypes = {
+  borderWidth: PropTypes.number
 }
