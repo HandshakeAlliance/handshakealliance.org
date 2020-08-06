966db43a60765237a6137c3de32bb4acbae205c1
JGeiser9 <32041734+JGeiser9@users.noreply.github.com>
2020-08-06 10:58:32 -0500

    Updates

---
+++ component---src-pages-about-js-be0f35a3081bda25d09d.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[5],{"3XHS":function(e,n,t){"use strict";t.r(n);var a=t("q1tI"),i=t.n(a),o=t("vOnD").c.section.withConfig({displayName:"about__Wrapper",componentId:"sc-1me1m18-0"})(["min-height:380px;padding-top:150px;width:90%;margin:0 auto;"]);n.default=function(){return i.a.createElement(o,null,i.a.createElement("h1",null,"About Us"),i.a.createElement("p",null,"We are the handshake alliance"))}}}]);
+//# sourceMappingURL=component---src-pages-about-js-be0f35a3081bda25d09d.js.map
